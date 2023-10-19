7d0d19f1194911482b836ea9ac1c7511ffb6f744
abbouformations <135717843+abbouformations@users.noreply.github.com>
2023-10-19 15:27:50 +0100

    Update ServiceImpl.java

--- src/main/java/ma/formations/rest/service/ServiceImpl.java
+++ src/main/java/ma/formations/rest/service/ServiceImpl.java
@@ -1,25 +1,19 @@
 package ma.formations.rest.service;
 
-import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import ma.formations.rest.dao.IDao;
 import ma.formations.rest.domaine.ArticleConverter;
 import ma.formations.rest.domaine.ArticleDTO;
 import ma.formations.rest.service.exception.BusinessException;
 import ma.formations.rest.service.service.model.Article;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@NoArgsConstructor
+@AllArgsConstructor
 public class ServiceImpl implements IService {
     private IDao dao;
-
-    @Autowired
-    public ServiceImpl(IDao dao) {
-        this.dao = dao;
-    }
 
     @Override
     public ArticleDTO getById(Long id) {
